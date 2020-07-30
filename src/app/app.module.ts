fb6dfa5686457c6ee3a966ae40ebbe649721d194
Pham Van Hieu <mr_hie@yahoo.com>
2020-07-30 08:39:11 +0800

    feat: finish Library and Premium

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,19 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { SuperTabsModule } from '@ionic-super-tabs/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot({ mode: 'ios', backButtonText: '' }), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot({ mode: 'ios', backButtonText: '' }),
+    AppRoutingModule,
+    SuperTabsModule.forRoot(),
+  ],
   providers: [
     StatusBar,
     SplashScreen,
